85c338872c4a5c9b63d6d71188c510e951abed26
antoniocorbibellot <antonio.corbi@ua.es>
2016-09-18 13:29:19 +0200

    First commit.

---
+++ source/app.d
@@ -0,0 +1,21 @@
+import std.stdio;
+import square;
+
+void main()
+{
+	writeln("Edit source/app.d to start your project.");
+
+    for (int n = 3; n < 22; n += 2) {
+      Square s = new Square (n);
+      s.computeValues;
+      writeln ("\n",s);
+      writefln ("sum row[0] = %d", s.sumRow(0));
+      writefln ("sum row[%d] = %d", n-1, s.sumRow(n-1));
+
+      writefln ("sum col[0] = %d", s.sumColumn(0));
+      writefln ("sum col[%d] = %d", n-1, s.sumColumn(n-1));
+
+      writefln ("sum diagA = %d", s.sumDiagA);
+      writefln ("sum diagB = %d", s.sumDiagB);
+    }
+}
---
+++ source/square.d
@@ -0,0 +1,106 @@
+module square;
+import std.stdio;
+
+class Square
+{
+  // The magick square
+  private int[][] matrix;
+
+  /**
+   * Parameters:
+   * n = must be odd.
+   */
+  public this (int n) {
+    assert (n > 0);
+    assert (n % 2 != 0);
+
+    matrix = new int[][n];    
+    foreach (ref r; matrix)  r = new int[n];
+  }
+
+  public void computeValues () {
+    int nr = cast(int) matrix.length;
+    int ne = cast(int) nr^^2;
+    int i = ((nr-1)/2)-1;
+    int j = ((nr+1)/2)-1;
+
+    // Set first pos to 1
+    matrix[i][j] = 1;
+    // And the rest of values.
+    for (int e = 2; e <= ne; e++)
+      {
+        i = i-1 < 0 ? nr-1 : i-1;
+        j = j+1 == nr ? 0 : j+1;
+        debug writeln ("Placing ",e,"@[", i, ",", j, "]");
+        if (matrix[i][j] != 0) { // New place is not free!
+          i = i-1 < 0 ? nr-1 : i-1;
+          j = j-1 < 0 ? nr-1 : j-1;
+        }
+        matrix[i][j] = e;
+      }
+  }
+  
+  public override string toString () {
+    import std.format;
+    import std.array : appender, replicate;
+    import std.range : repeat;
+    
+    auto writer = appender!string();
+    auto n = matrix.length;
+    
+    formattedWrite (writer, "[%d X %d]\n", n, n);
+    formattedWrite (writer, "-".replicate(9*matrix.length)~"\n");
+    for(int r = 0 ; r < matrix.length; r++) {
+      for (int c = 0 ; c < matrix[r].length; c++) {
+        formattedWrite (writer, "[%4s]", matrix[r][c]);
+        //writeln ("ncol: ", matrix[r][c]);
+      }
+      formattedWrite (writer, "\n");
+    }
+    formattedWrite (writer, "-".replicate(9*matrix.length)~"\n");
+    
+    return writer.data;
+  }
+
+  public size_t sumColumn (int c) {
+    assert ((c < matrix.length) && (matrix.length > 0));
+
+    size_t s;
+    for (int r = 0; r < matrix.length; r++)
+      s += matrix[r][c];
+
+    return s;
+  }
+  public size_t sumRow (int r) {
+    assert ((r < matrix.length) && (matrix.length > 0));
+
+    size_t s;
+    for (int c = 0; c < matrix.length; c++)
+      s += matrix[r][c];
+
+    return s;
+  }
+  public size_t sumDiagA () {
+    assert (matrix.length > 0);
+
+    size_t s;
+    for (int i = 0; i < matrix.length; i++)
+      s += matrix[i][i];
+
+    return s;
+  }
+  public size_t sumDiagB () {
+    assert (matrix.length > 0);
+
+    int c = cast(int) matrix.length - 1;
+    size_t s;
+    for (int i = 0; i < matrix.length; i++) {
+      s += matrix[i][c-i];
+      //writefln ("matrix[%d][%d]=%d, s = %d", i, c-i, matrix[i][c-i], s);
+    }
+
+    //writefln ("diagB returns: %d", s);
+    return s;
+  }
+
+}
